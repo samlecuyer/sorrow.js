51f83f7db7d5c25cf8d4a4ef0080cdf0276016bf
Sam L'Ecuyer <samlecuyer@gmail.com>
2012-04-01 13:08:45 -0700

    add base and extension to FS

--- test/testFs.js
+++ test/testFs.js
@@ -40,6 +40,42 @@ test.run({
         equal(path, joinPath, 'the paths should be identical');
     },
     
+    testFilesystem_base: function() {
+        var path = '/web/home/sam.js';
+        equal('sam.js', fs.base(path));
+    },
+
+    testFilesystem_base2: function() {
+        var path = '/web/home/sam.js';
+        equal('sam', fs.base(path, 'js'));
+    },
+    
+    testFilesystem_base3: function() {
+        var path = '/web/home/sam.js.ext';
+        equal('sam.js.ext', fs.base(path, 'js'));
+    },
+    
+    testFilesystem_extension: function() {
+        var path = '/web/home/sam.js.ext';
+        equal('ext', fs.extension(path));
+    },
+    
+    testFilesystem_extension2: function() {
+        var path = '/web/home/sam';
+        equal('', fs.extension(path));
+    },
+    
+    testFilesystem_Path: function() {
+        var path = new fs.Path('/web/home/sam');
+        equal('/web/home/sam', path);
+    },
+    
+    testFilesystem_Path2: function() {
+        var path = new fs.Path('/web/home', 'sam');
+        equal('/web/home/sam', path);
+    },
+
+    
     testModule_require: function() {
         var yo = require('./a').yo('This shouldnt throw an exception');
         equal(5, yo, 'Should be 5');
